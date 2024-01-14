f7d1dc649b66ab55cb2fa7a9d55d40c81aefac72
HerndonE <EHerndon17@gmail.com>
2024-01-13 18:43:12 -0700

    [update] refactor weatherapi script, readme

--- Code/weatherapi.py
+++ Code/weatherapi.py
@@ -1,103 +1,62 @@
 #!/usr/bin/python3
 
-'''
-Ethan Herndon
-SSMTP Weather to Text Message via Python
-5.23.2020
-'''
+"""
+Name:        Ethan Herndon
+Description: SSMTP Weather to Text Message via Python
+Date:        1.13.24
+Reference:   https://openweathermap.org/api
+             https://www.epochconverter.com/timezones
+"""
 
 import requests
 import time
 import smtplib
 
-''' Get Current Weather When Requested'''
-api_address = 'https://api.openweathermap.org/data/2.5/weather?appid=YOURAPIKEY&q='
-city = "YOUR TOWN HERE"
-url = api_address + city + '&units=imperial' # You can also use 'metric' depending on your location
-json_data = requests.get(url).json()
 
-formatted_data = json_data['weather'][0]['main']
-weather_desc = "Description: " + json_data['weather'][0]['description']
-temp = (json_data['main']['temp'])
-temp_max = (json_data['main']['temp_max'])
-feel_temp = (json_data['main']['feels_like'])
-
-offsetInSeconds = some number #Relative to UTC/GMT. You can tailor this to your time zone
-# Reference: https://www.epochconverter.com/timezones
-
-sunup = (json_data['sys']['sunrise'] - offsetInSeconds) # In my case I am behind UTC/GMT otherwise use '+'
-'''sunrise = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(sunup))'''
-sunrise = time.strftime("%H:%Mam", time.localtime(sunup))
-sundown = (json_data['sys']['sunset'] - offsetInSeconds)
-'''sunset = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(sundown))'''
-sunset = time.strftime("%H:%Mpm", time.localtime(sundown))
+def weather_setup(city):
+    api_address = f"https://api.openweathermap.org/data/2.5/weather?appid=YOURAPIKEY&q="  # Get current weather when
+    # requested
+    url = f"{api_address}{city}&units=imperial"  # Change to 'metric' depending on country of origin
+    return url
 
 
-'''print("The weather in " + city + " is " + "\n") # Test print the weather in console
-print(formatted_data)
-print(weather_desc)
-print("Current Temperature: " + str(temp) + "F")
-print("Feels Like: " + str(feel_temp) + "F")
-print("Maximum Temperature: " + str(temp_max) + "F")
-print("Sunrise: " + sunrise)
-print("Sunset: " + sunset)'''
+def weather_print(message, sender_email, sender_password, end_user):
+    mail = smtplib.SMTP('smtp.gmail.com', 587)
+    mail.ehlo()
+    mail.starttls()
+    mail.login(f"{sender_email}", f"{sender_password}")
+    mail.sendmail(f"{sender_email}", f"{end_user}", message)
+    mail.close()
 
-one = city + " weather is " + "\n" # Assigned desired output as variables to be concatenated
-two = formatted_data + "\n"
-three = weather_desc + "\n"
-four = "Current Temp: " + str(int(temp)) + "F" + "\n"
-five = "Feels Like: " + str(int(feel_temp)) + "F" + "\n"
-six = "Max Temp: " + str(int(temp_max)) + "F" + "\n"
-eight = "Sunset: " + sunset + "\n"
-seven = "Sunrise: " + sunrise + "\n" 
 
-content = one + two + three + four + five + six + seven + eight
-mail = smtplib.SMTP('smtp.gmail.com',587)
-mail.ehlo()
-mail.starttls()
-mail.login('SENDER EMAIl','SENDER PASSWORD')
-mail.sendmail('SENDER EMAIL','RECIEVER',content)
-mail.close()
+def weather_main():
+    city = f"YOUR TOWN HERE"
+    weather_api_url = weather_setup(city)
+    json_data = requests.get(weather_api_url).json()
 
-''' Send User 5 Day Forecast Each Day At 5pm '''
-api_address = 'https://api.openweathermap.org/data/2.5/forecast?appid=YOURAPIKEY&q='
-city = "YOUR TOWN HERE"
-url = api_address + city + '&units=imperial'
-json_data = requests.get(url).json()
+    formatted_data = json_data['weather'][0]['main']
+    weather_desc = f"Description: {json_data['weather'][0]['description']}"
+    temp = (json_data['main']['temp'])
+    temp_max = (json_data['main']['temp_max'])
+    feel_temp = (json_data['main']['feels_like'])
 
-day1 = (json_data['list'][3]['dt'] - offsetInSeconds) 
-dayOne = time.strftime("%m-%d @ %Hpm", time.localtime(day1))
-tempOne = (json_data['list'][3]['main']['temp'])
+    # some number relative to UTC/GMT. You can tailor this to your time zone https://www.epochconverter.com/timezones
+    offset_in_seconds = "Offset In seconds HERE"
 
-day2 = (json_data['list'][11]['dt'] - offsetInSeconds)
-dayTwo = time.strftime("%m-%d @ %Hpm", time.localtime(day2))
-tempTwo = (json_data['list'][11]['main']['temp'])
+    # In my case I am behind UTC/GMT otherwise use '+', time format ex:
+    # time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
+    sunup = (json_data['sys']['sunrise'] - offset_in_seconds)
+    sunrise = time.strftime("%H:%Mam", time.localtime(sunup))
+    sundown = (json_data['sys']['sunset'] - offset_in_seconds)
+    sunset = time.strftime("%H:%Mpm", time.localtime(sundown))
 
-day3 = (json_data['list'][19]['dt'] - offsetInSeconds)
-dayThree = time.strftime("%m-%d @ %Hpm", time.localtime(day3))
-tempThree = (json_data['list'][19]['main']['temp'])
+    sender_email, sender_password, end_user = "", "", ""
+    # Assigned variables into a desired output
+    message = f"{city} weather is \n{formatted_data}\n{weather_desc}\nCurrent Temp: {str(int(temp))}F\nFeels Like: " \
+              f"{str(int(feel_temp))}F\nMax Temp: {str(int(temp_max))}F\nSunset: {sunset}\nSunrise: {sunrise}"
 
-day4 = (json_data['list'][27]['dt'] - offsetInSeconds)
-dayFour = time.strftime("%m-%d @ %Hpm", time.localtime(day4))
-tempFour = (json_data['list'][27]['main']['temp'])
+    weather_print(message, sender_email, sender_password, end_user)
 
-day5 = (json_data['list'][35]['dt'] - offsetInSeconds)
-dayFive = time.strftime("%m-%d @ %Hpm", time.localtime(day5))
-tempFive = (json_data['list'][35]['main']['temp'])
 
-forecast = "5 Day Forecast \n" # Assigned Forecast output as variables to be concatenated
-p1 = "Temp: " + str(int(tempOne)) + "F \n"
-p2 = "Temp: " + str(int(tempTwo)) + "F \n"
-p3 = "Temp: " + str(int(tempThree)) + "F \n"
-p4 = "Temp: " + str(int(tempFour)) + "F \n"
-p5 = "Temp: " + str(int(tempFive)) + "F \n"
-
-content = forecast + dayOne +"\n" + p1 + dayTwo +"\n" + p2 + dayThree +"\n" + p3 + dayFour +"\n" + p4 + dayFive +"\n" + p5
-mail = smtplib.SMTP('smtp.gmail.com',587)
-mail.ehlo()
-mail.starttls()
-mail.login('SENDER EMAIl','SENDER PASSWORD')
-mail.sendmail('SENDER EMAIL','RECIEVER',content)
-mail.close()
-
-# Reference: https://openweathermap.org/api
+if __name__ == '__main__':
+    weather_main()
